74c8713ec8a32d3fd599fda046eda04689094b47
Mark Junker <m.junker@fubar-dev.de>
2015-06-01 17:20:45 +0200

    Started conversion of the CSV format

---
+++ BeanIO/Builder/CsvParserBuilder.cs
@@ -0,0 +1,13 @@
+﻿using BeanIO.Internal.Config;
+using BeanIO.Stream;
+
+namespace BeanIO.Builder
+{
+    public class CsvParserBuilder : IParserBuilder
+    {
+        public BeanConfig<IRecordParserFactory> Build()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ BeanIO/Stream/Csv/CsvRecordParserFactory.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+
+namespace BeanIO.Stream.Csv
+{
+    public class CsvRecordParserFactory : CsvParserConfiguration, IRecordParserFactory
+    {
+        /// <summary>
+        /// Initializes the factory.
+        /// </summary>
+        /// <remarks>
+        /// This method is called when a mapping file is loaded after
+        /// all parser properties have been set, and is therefore ideally used to preemptively
+        /// validate parser configuration settings.
+        /// </remarks>
+        public void Init()
+        {
+            if (Quote == Delimiter)
+                throw new BeanIOConfigurationException("The CSV field delimiter cannot match the character used for the quotation mark.");
+
+            if (Escape != null && Escape == Delimiter)
+                throw new BeanIOConfigurationException("The CSV field delimiter cannot match the escape character.");
+        }
+
+        /// <summary>
+        /// Creates a parser for reading records from an input stream.
+        /// </summary>
+        /// <param name="reader">The input stream to read from</param>
+        /// <returns>The created <see cref="IRecordReader"/></returns>
+        public IRecordReader CreateReader(TextReader reader)
+        {
+            return new CsvReader(reader);
+        }
+
+        /// <summary>
+        /// Creates a parser for writing records to an output stream.
+        /// </summary>
+        /// <param name="writer">The output stream to write to</param>
+        /// <returns>The new <see cref="IRecordWriter"/></returns>
+        public IRecordWriter CreateWriter(TextWriter writer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Creates a parser for marshalling records.
+        /// </summary>
+        /// <returns>The created <see cref="IRecordMarshaller"/></returns>
+        public IRecordMarshaller CreateMarshaller()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Creates a parser for unmarshalling records.
+        /// </summary>
+        /// <returns>The created <see cref="IRecordUnmarshaller"/></returns>
+        public IRecordUnmarshaller CreateUnmarshaller()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
