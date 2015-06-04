478d4af432d06d8849514d077182054cd0f9a9ff
Mark Junker <m.junker@fubar-dev.de>
2015-06-04 10:00:38 +0200

    Ensure that the string instance is always the same (and unique)

--- BeanIO/Internal/Parser/Value.cs
+++ BeanIO/Internal/Parser/Value.cs
@@ -5,16 +5,16 @@
         /// <summary>
         /// Constant indicating the field was not present in the stream
         /// </summary>
-        public const string Missing = "-missing-";
+        public static readonly string Missing = new string("-missing-".ToCharArray());
 
         /// <summary>
         /// Constant indicating the field did not pass validation
         /// </summary>
-        public const string Invalid = "-invalid-";
+        public static readonly string Invalid = new string("-invalid-".ToCharArray());
 
         /// <summary>
         /// Constant indicating the field was nil (XML only)
         /// </summary>
-        public const string Nil = "-nil-";
+        public static readonly string Nil = new string("-nil-".ToCharArray());
     }
 }
