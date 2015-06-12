1802e1c486a2974c07c9863a30dc3ef323a6a0dd
Mark Junker <m.junker@fubar-dev.de>
2015-06-12 11:26:55 +0200

    Conversion of configuration tests

---
+++ BeanIO.Test/Config/XmlMappingConfigurationTest.cs
@@ -0,0 +1,29 @@
+﻿using BeanIO.Parser;
+
+using Xunit;
+
+namespace BeanIO.Config
+{
+    public class XmlMappingConfigurationTest : ParserTest
+    {
+        [Fact]
+        public void TestTemplateImport()
+        {
+            var factory = StreamFactory.NewInstance();
+            using (var stream = typeof(ParserTest).Assembly.GetManifestResourceStream("BeanIO.Config.ab.xml"))
+            {
+                factory.Load(stream);
+            }
+        }
+
+        [Fact]
+        public void TestImport()
+        {
+            var factory = StreamFactory.NewInstance();
+            using (var stream = typeof(ParserTest).Assembly.GetManifestResourceStream("BeanIO.Config.import.xml"))
+            {
+                factory.Load(stream);
+            }
+        }
+    }
+}
