0e23ea1932b114e8bf5ede31ec6db07f181b6db4
Mark Junker <m.junker@fubar-dev.de>
2015-06-15 09:37:39 +0200

    Added test cases

---
+++ BeanIO.Test/Parser/Constructor/Color.cs
@@ -0,0 +1,46 @@
+﻿namespace BeanIO.Parser.Constructor
+{
+    public class Color
+    {
+        private readonly string _name;
+
+        private readonly int _r;
+
+        private readonly int _g;
+
+        private readonly int _b;
+
+        public Color()
+            : this("black", 0, 0, 0)
+        {
+        }
+
+        public Color(string name, int r, int g, int b)
+        {
+            _name = name;
+            _r = r;
+            _g = g;
+            _b = b;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public int R
+        {
+            get { return _r; }
+        }
+
+        public int G
+        {
+            get { return _g; }
+        }
+
+        public int B
+        {
+            get { return _b; }
+        }
+    }
+}
