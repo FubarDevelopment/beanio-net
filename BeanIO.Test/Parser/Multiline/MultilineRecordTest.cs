345de6458b17659b8b82e48c3fef4c126ab53385
Mark Junker <m.junker@fubar-dev.de>
2015-06-16 17:20:10 +0200

    More tests

--- BeanIO.Test/Parser/Multiline/MultilineRecordTest.cs
+++ BeanIO.Test/Parser/Multiline/MultilineRecordTest.cs
@@ -99,7 +99,7 @@ namespace BeanIO.Parser.Multiline
         }
 
         [Fact]
-        public void TestNestedRecorGroup()
+        public void TestNestedRecordGroup()
         {
             var factory = NewStreamFactory("BeanIO.Parser.Multiline.multiline_mapping.xml");
             var reader = factory.CreateReader("ml2", LoadStream("ml2.txt"));
@@ -139,7 +139,7 @@ namespace BeanIO.Parser.Multiline
         }
 
         [Fact]
-        public void TestNestedRecorGroupCollections()
+        public void TestNestedRecordGroupCollections()
         {
             var factory = NewStreamFactory("BeanIO.Parser.Multiline.multiline_mapping.xml");
             var reader = factory.CreateReader("ml3", LoadStream("ml3.txt"));
@@ -147,12 +147,134 @@ namespace BeanIO.Parser.Multiline
             {
                 // read batch #1
                 var map = Assert.IsType<Dictionary<string, object>>(reader.Read());
-                throw new NotImplementedException();
+                Assert.True(map.ContainsKey("batch"));
+                var list = Assert.IsType<List<OrderBatch>>(map["batch"]);
+                Assert.Collection(
+                    list,
+                    orderList =>
+                        {
+                            Assert.Collection(
+                                orderList.Orders,
+                                order => Assert.Equal("100", order.Id),
+                                order => Assert.Equal("101", order.Id));
+                        },
+                    orderList =>
+                        {
+                            Assert.Collection(
+                                orderList.Orders,
+                                order => Assert.Equal("103", order.Id));
+                        });
+
+                var text = new StringWriter();
+                factory.CreateWriter("ml3", text).Write(map);
+                Assert.Equal(
+                    "header,2\n" +
+                    "order,100,2012-01-01\n" +
+                    "customer,George,Smith\n" +
+                    "order,101,2012-01-01\n" +
+                    "customer,John,Smith\n" +
+                    "header,1\n" +
+                    "order,103,2012-01-01\n" +
+                    "customer,Jen,Smith\n",
+                    text.ToString());
             }
             finally
             {
                 reader.Close();
             }
+        }
+
+        [Fact]
+        public void TestRecordMap()
+        {
+            var factory = NewStreamFactory("BeanIO.Parser.Multiline.multiline_mapping.xml");
+            var reader = factory.CreateReader("ml4", LoadStream("ml4.txt"));
+            try
+            {
+                // read order #1
+                var order = Assert.IsType<Order>(reader.Read());
+
+                var itemMap = order.ItemMap;
+                Assert.Collection(
+                    itemMap,
+                    item =>
+                        {
+                            Assert.Equal("soda", item.Key);
+                            Assert.Equal("soda", item.Value.Name);
+                            Assert.Equal(2, item.Value.Quantity);
+                        },
+                    item =>
+                        {
+                            Assert.Equal("carrots", item.Key);
+                            Assert.Equal("carrots", item.Value.Name);
+                            Assert.Equal(5, item.Value.Quantity);
+                        });
+
+                var text = new StringWriter();
+                var writer = factory.CreateWriter("ml4", text);
+
+                writer.Write(order);
+
+                order = Assert.IsType<Order>(reader.Read());
+                Assert.NotNull(order.ItemMap);
+                Assert.Equal(3, order.ItemMap.Count);
+
+                writer.Write(order);
+
+                writer.Flush();
+
+                Assert.Equal(
+                    "order,100,2012-01-01\n" +
+                    "item,soda,2\n" +
+                    "item,carrots,5\n" +
+                    "order,101,2012-01-01\n" +
+                    "item,banana,1\n" +
+                    "item,apple,2\n" +
+                    "item,cereal,3\n",
+                    text.ToString());
+            }
+            finally
+            {
+                reader.Close();
+            }
+        }
+
+        [Fact]
+        public void TestNestedRecordGroupNonCollection()
+        {
+            var factory = NewStreamFactory("BeanIO.Parser.Multiline.multiline_mapping.xml");
+            var reader = factory.CreateReader("ml5", LoadStream("ml5.txt"));
+            try
+            {
+                var batch = Assert.IsType<OrderBatch>(reader.Read());
+                Assert.Equal(2, batch.BatchCount);
+
+                var order = batch.Order;
+                Assert.NotNull(order);
+                Assert.Equal("100", order.Id);
+
+                var customer = order.Customer;
+                Assert.NotNull(customer);
+                Assert.Equal("George", customer.FirstName);
+
+                var text = new StringWriter();
+                factory.CreateWriter("ml5", text).Write(batch);
+                Assert.Equal(
+                    "header,2\n" +
+                    "order,100,2012-01-01\n" +
+                    "customer,George,Smith\n",
+                    text.ToString());
+            }
+            finally
+            {
+                reader.Close();
+            }
+        }
+
+        [Fact]
+        public void TestEmptyRecordList()
+        {
+            throw new NotImplementedException();
         }
 
         private static TextReader LoadStream(string fileName)
