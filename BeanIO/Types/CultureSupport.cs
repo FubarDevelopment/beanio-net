f21fdd8dce5a24fe61212d8fe6e1a8e4537af3f1
Mark Junker <m.junker@fubar-dev.de>
2015-06-17 16:41:50 +0200

    Added support for "locale" configuration

--- BeanIO/Types/CultureSupport.cs
+++ BeanIO/Types/CultureSupport.cs
@@ -1,15 +1,24 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 
 namespace BeanIO.Types
 {
     public abstract class CultureSupport
     {
-        private CultureInfo _culture = CultureInfo.CurrentCulture;
+        private static readonly CultureInfo _cultureEnUs = new CultureInfo("en-US");
+
+        private CultureInfo _culture = _cultureEnUs;
 
         public CultureInfo Culture
         {
             get { return _culture; }
             set { _culture = value; }
+        }
+
+        public string Locale
+        {
+            get { return _culture.Name; }
+            set { _culture = new CultureInfo(value); }
         }
     }
 }
