09763f5ed42964069be3d96eb78df15b126efc62
Mark Junker <m.junker@fubar-dev.de>
2015-07-13 13:22:51 +0200

    Added a new GlobalAssemblyInfo that's shared between all projects.

--- BeanIO/Properties/AssemblyInfo.cs
+++ BeanIO/Properties/AssemblyInfo.cs
@@ -1,32 +1,8 @@
 ﻿using System.Reflection;
-using System.Resources;
 using System.Runtime.CompilerServices;
 
-// Allgemeine Informationen über eine Assembly werden über folgende
-// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-// die einer Assembly zugeordnet sind.
 [assembly: AssemblyTitle("BeanIO")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Fubar Development")]
+[assembly: AssemblyDescription("Framework for marshalling and unmarshalling .NET objects")]
 [assembly: AssemblyProduct("BeanIO")]
-[assembly: AssemblyCopyright("Copyright © Fubar Development, Kevin Seim 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern
-// durch Einsatz von '*', wie in nachfolgendem Beispiel:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.0")]
-[assembly: AssemblyFileVersion("2.2.1")]
-[assembly: AssemblyInformationalVersion("2.2.1")]
 
 [assembly: InternalsVisibleTo("BeanIO.Test")]
---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Fubar Development")]
+[assembly: AssemblyCopyright("Copyright © Fubar Development, Kevin Seim 2015")]
+//// [assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion("2.2.0")]
+[assembly: AssemblyFileVersion("2.2.1")]
+[assembly: AssemblyInformationalVersion("2.2.1")]
