f3c1f256c36f1cd63d24ecaf1e76b266d556e31d
Mark Junker <m.junker@fubar-dev.de>
2016-07-18 15:35:26 +0200

    Fixed project and package names

--- pack.cmd
+++ pack.cmd
@@ -1,6 +1,6 @@
 @echo off
 if exist "package-output" del "package-output" /f /q
-for /d %%D in (src\BeanIO*) do (
+for /d %%D in (src\*) do (
 	dotnet pack %%D -c Release -o "package-output" --no-build --version-suffix "beta1-%1"
 	if %errorlevel% neq  0 exit /b %errorlevel%
 )
