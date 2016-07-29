5759d6db85579193bc291b8710dce5cc63d4c2ac
Mark Junker <m.junker@fubar-dev.de>
2016-07-29 13:36:40 +0200

    Added a PS script to build the NuGet package

---
+++ pack-nuget.ps1
@@ -0,0 +1,21 @@
+param(
+	[Parameter(Mandatory=$True)]
+	[string]$Version,
+	[string]$VersionSuffix = $null
+)
+
+$packageOutputPath = Join-Path $PSScriptRoot "package-output"
+if (test-path $packageOutputPath) {
+	remove-item -whatif -recurse -force $packageOutputPath
+}
+
+$fullVersion = $Version + (&{if ($VersionSuffix) { "-" + $VersionSuffix } else { "" }})
+
+$nuspecPath = join-path (Join-Path $PSScriptRoot "packaging") "*.nuspec"
+$nugetPath = join-path (Join-Path $PSScriptRoot "packaging") "NuGet.exe"
+
+get-item $nuspecPath | % {
+	$packageId = $_.Basename
+	&$nugetPath pack $_.FullName -Properties "Configuration=Release;Id=$packageId" -OutputDirectory $packageOutputPath -Version $fullVersion
+	if (!$?) { exit 1 }
+}
